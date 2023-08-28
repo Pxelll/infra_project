40f7bec8482c44a5dce3b67f14bf61fcbbca9897
adam <adam@adam.myguest.virtualbox.org>
2023-08-27 21:15:22 -0300

    add name of instances of prod

--- env/prod/main.tf
+++ env/prod/main.tf
@@ -1,11 +1,11 @@
 module "aws-prod" {
-  source = "../../infra"
-  instance_type = "t2.micro" # para producao eu posso trocar mais tarde a maquina
-  aws_region = "us-east-1" # posso trocar tambem a regiao mas nao tem necessidade para aprendizagem
-  key = "aws-key-prod"
-  ec2_name = "ec2_prod"
+  source              = "../../infra"
+  instance_type       = "t2.micro"  # para producao eu posso trocar mais tarde a maquina
+  aws_region          = "us-east-1" # posso trocar tambem a regiao mas nao tem necessidade para aprendizagem
+  key                 = "aws-key-prod"
+  ec2_name            = "prod"
   security_group_name = "PROD"
-  max_ec2 = 10
-  min_ec2 = 1
-  group_name = "PROD"
+  max_ec2             = 10
+  min_ec2             = 1
+  group_name          = "PROD"
 }
