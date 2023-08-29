6c86d0da03e47f7fe0c70cc654328ad41f279318
adam <adam@adam.myguest.virtualbox.org>
2023-08-29 11:57:17 -0300

    last commit

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -4,12 +4,9 @@ module "aws-dev" {
   aws_region = "us-east-1"
   key = "aws-key-dev"
   ec2_name = "ec2_dev"
-  security_group = "DEV"
+  security_group_name = "DEV"
   group_name = "DEV"
   min_ec2 = 0
   max_ec2 = 1
-}
-
-output "public_ip_dev" {
-  value = module.aws-dev.public_ip
+  production = false
 }
